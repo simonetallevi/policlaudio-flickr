db8c64cea0597d421d3607e8ece24e1a06df82f4
Simone Tallevi-Diotallevi <simone.tallevi@pirelli.com>
2018-04-30 16:40:57 +0200

    adding gridlist component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,11 @@ import 'hammerjs';
 })
 export class AppComponent {
   title = 'app';
+
+  tiles = [
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 2, rows: 1, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     MatButtonModule, 
     MatCheckboxModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatGridListModule
   ],
   exports: [
     BrowserAnimationsModule,
     MatButtonModule, 
     MatCheckboxModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatGridListModule
   ],
 })
 export class MaterialModule { }
